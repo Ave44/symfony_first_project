27c6edca529e7a7b51fb532abfb74b378f342a65
Paweł Olszewski <monte.paw@gmail.com>
2025-11-02 20:51:19 +0100

    implement CRUD for "ShopItem" class

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\ShopItem;
+use App\Form\ShopItemFormType;
 use App\Repository\ShopItemRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,6 +24,26 @@ final class ProductsController extends AbstractController
         ]);
     }
 
+    #[Route('products/add', name: 'product_add')]
+    public function addProduct(Request $request, EntityManagerInterface $manager): Response
+    {
+        $shopItem = new ShopItem();
+        $shopItemForm = $this->createForm(ShopItemFormType::class, $shopItem);
+
+        $shopItemForm->handleRequest($request);
+
+        if ($shopItemForm->isSubmitted()) {
+            $manager->persist($shopItem);
+            $manager->flush();
+
+            return $this->redirectToRoute('product_details', ['productId' => $shopItem->getId()]);
+        }
+
+        return $this->render('products/productAdd.html.twig', [
+            'shopItemForm' => $shopItemForm,
+        ]);
+    }
+
     #[Route('/products/{productId}', name: 'product_details')]
     public function productDetails(string $productId, ShopItemRepository $shopItemRepository): Response
     {
@@ -28,5 +51,37 @@ final class ProductsController extends AbstractController
         return $this->render('products/productDetails.html.twig', [
             'shopItem' => $shopItem,
         ]);
+    }
+
+    #[Route('products/{productId<\d+>}/edit', name: 'product_edit')]
+    public function editProduct(string $productId, Request $request, EntityManagerInterface $manager, ShopItemRepository $shopItemRepository): Response
+    {
+        $shopItem = $shopItemRepository->find($productId);
+        $shopItemForm = $this->createForm(ShopItemFormType::class, $shopItem);
+
+        $shopItemForm->handleRequest($request);
+
+        if ($shopItemForm->isSubmitted()) {
+            $manager->flush();
+
+            return $this->redirectToRoute('product_details', ['productId' => $shopItem->getId()]);
+        }
+
+        return $this->render('products/productEdit.html.twig', [
+            'shopItemForm' => $shopItemForm,
+            'shopItem' => $shopItem,
+        ]);
+    }
+
+    #[Route('products/{productId<\d+>}/delete', name: 'product_delete')]
+    public function deleteProduct(string $productId, Request $request, EntityManagerInterface $manager, ShopItemRepository $shopItemRepository) {
+        if ($request->isMethod('POST')) {
+            $shopItem = $shopItemRepository->find($productId);
+            if ($shopItem) {
+                $manager->remove($shopItem);
+                $manager->flush();
+            }
+            return $this->redirectToRoute('app_products');
+        }
     }
 }
---
+++ src/Form/ShopItemFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ShopItem;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ShopItemFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('availableItems')
+            ->add('price')
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ShopItem::class,
+        ]);
+    }
+}
